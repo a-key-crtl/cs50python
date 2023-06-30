1a841c40e0c20a99b1a22316d52b7da756022ba6
Alexander Key <keyalexander99@gmail.com>
2023-06-29 21:57:33 -0700

    Create user input for camelCase inserted for loop check each letter upper case.

---
+++ pset2/camel.py
@@ -0,0 +1,22 @@
+# This program will prompt user for name of a 
+# variable in camel case and output corresponding name in 
+# snake case
+
+# Get user input
+camelCase = input("camelCase: ")
+
+# Print "snake_case: "
+print("snake_case: ", end="")
+
+# Loop through every letter
+for letter in camelCase:
+    
+
+    # Check if letter is upper case
+    if letter.isupper(): 
+
+        # Print underscores and the letter in lowercase
+
+    # Otherwise, print letter
+
+# Print space in the end
