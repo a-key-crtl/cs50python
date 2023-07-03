729e2edbe170aaf022d52aff533ed5e76fb6ef10
Alexander Key <keyalexander99@gmail.com>
2023-07-02 22:16:25 -0700

    Cleaned up code syntax to look cleaner in programm

--- pset2/plates.py
+++ pset2/plates.py
@@ -29,7 +29,7 @@ def is_valid(s):
         i += 1
     # No periods, spaces, or punctuation marks allowed
     for d in s:
-        if d in ['.',' ' ,'!' , '?']:
+        if d in ['.', ' ' , '!', '?']:
             return False
     
     # Pass all the tests return true
